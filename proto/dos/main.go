be718c55290870f7105165aa657fc4a5fba7238d
elmunyeco <elyecomun@gmail.com>
2020-02-14 20:37:15 -0300

    tested persistence. composite negated

---
+++ proto/dos/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"os"
+	//"github.com/munye/prueba_backend_go/common"
+	"strconv"
+)
+
+// Estudio describes the behavior that needs to be exercised uniformly
+// across all primitive and composite objects.
+type Componente interface {
+	Traverse()
+}
+
+// Campo describes a primitive leaf object in the hierarchy.
+type Campo struct {
+	gorm.Model `json:"campo"`
+	EstudioID  int
+	Value      string `gorm:"column:value" json:"valor"`
+}
+
+// NewCampo creates a new leaf.
+func NewCampo(value string) *Campo {
+	return &Campo{Value: value}
+}
+
+// Traverse prints the value of the leaf.
+func (c *Campo) Traverse() {
+	js, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(js))
+}
+
+// Estudio describes a composite of components.
+type Estudio struct {
+	gorm.Model `json:"estudio"`
+	PacienteID uint    `json:"paciente_id"`
+	Campos     []Campo `gorm:"type:Campos" json:"campos"`
+}
+
+// NewEstudio creates a new composite.
+func NewEstudio(pacienteID uint) *Estudio {
+	return &Estudio{PacienteID: pacienteID, Campos: make([]Campo, 0)}
+}
+
+// Add adds a new component to the composite.
+func (e *Estudio) Add(campo *Campo) {
+	e.Campos = append(e.Campos, *campo)
+}
+
+// Traverse traverses the composites campos.
+func (e *Estudio) Traverse() {
+	js, err := json.Marshal(e)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(js))
+}
+
+func initialMigration(db *gorm.DB) {
+	db.AutoMigrate(&Estudio{})
+	db.AutoMigrate(&Campo{})
+}
+
+func main() {
+
+	os.Remove("sqlite3gorm.db")
+	db, err := gorm.Open("sqlite3", "sqlite3gorm.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("fallo la conexión. a la concha de tu madre")
+	}
+	defer db.Close()
+	initialMigration(db)
+
+	estudio := NewEstudio(1)
+
+	for i := 0; i < 10; i++ {
+		campo := NewCampo(strconv.Itoa(i))
+		estudio.Add(campo)
+	}
+	db.Create(estudio)
+
+	estudio.Traverse()
+
+}
