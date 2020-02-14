02ed681b580b6595bfad1c35a836b339680a9c40
elmunyeco <elyecomun@gmail.com>
2020-02-14 17:31:49 -0300

    deleted dos y tres

--- proto/uno/main.go
+++ proto/uno/main.go
@@ -3,48 +3,68 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"time"
+	"strconv"
 )
 
-type Paciente struct {
-	Nombre   string `json:"nombre"`
-	Estudios []Estudio
+// Estudio describes the behavior that needs to be exercised uniformly
+// across all primitive and composite objects.
+type Componente interface {
+	Traverse()
 }
 
+// Campo describes a primitive leaf object in the hierarchy.
+type Campo struct {
+	Value string `json:valor`
+}
+
+// NewCampo creates a new leaf.
+func NewCampo(value string) *Campo {
+	return &Campo{Value: value}
+}
+
+// Traverse prints the value of the leaf.
+func (c *Campo) Traverse() {
+	js, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(js))
+}
+
+// Estudio describes a composite of components.
 type Estudio struct {
-	CampoA string `json:campo_a`
-	CampoB string `json:campo_b`
-	CampoC string `json:campo_c`
+	PacienteID uint         `json:paciente_id`
+	Campos     []Componente `json:campos`
 }
 
-type Visita struct {
-	Fecha    time.Time `json:fecha`
-	Paciente Paciente
+// NewEstudio creates a new composite.
+func NewEstudio(pacienteID uint) *Estudio {
+	return &Estudio{PacienteID: pacienteID, Campos: make([]Componente, 0)}
 }
 
-func main() {
-	p := Paciente{
-		Nombre: "Juan Jose Paso",
-		Estudios: []Estudio{
-			Estudio{
-				CampoA: "Bien",
-				CampoB: "Bien",
-				CampoC: "Bien",
-			},
-			Estudio{
-				CampoA: "Para el orto",
-				CampoB: "Se murio",
-				CampoC: "cago fuego",
-			},
-		},
-	}
-	v := Visita{
-		Paciente: p,
-		Fecha:    time.Now(),
-	}
-	js, err := json.Marshal(v)
+// Add adds a new component to the composite.
+func (e *Estudio) Add(componente Componente) {
+	e.Campos = append(e.Campos, componente)
+}
+
+// Traverse traverses the composites campos.
+func (e *Estudio) Traverse() {
+	js, err := json.Marshal(e)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(js))
+}
+
+func main() {
+
+	estudio := NewEstudio(1)
+
+	for i := 0; i < 10; i++ {
+		estudio.Add(NewCampo(strconv.Itoa(i)))
+	}
+
+	estudio.Traverse()
+
 }
